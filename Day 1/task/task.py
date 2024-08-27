29714ccba4e51fc83df9ec9d39d5722001b56822
manda <mandard.4585@gmail.com>
2024-08-27 20:40:17 +0530

    Update latest

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,28 @@
+
+# Len command
+
+#username = input("What is Your name? \n")
+#length = len(username)
+#print(length)
+
+
+
+
+
+#Variable exercise to swap milk and juice
+
+#glass1 = "milk"
+#glass2 = "juice"
+
+#glass3 = glass2
+#glass2 = glass1
+#glass1 = glass3
+
+
+
+
+#Day 1 Final Project
+
 print("Welcome to Band Name Generator.\n")
 city = input("Whats the name of the city you grew up in?\n")
 pet_name = input("What's your pet's name?\n")
